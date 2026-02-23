aa861ced80320ce5b9af3f5cd7fc14f41ac8be22
Joel Takahashi Olsson <joel.t.t.o@gmail.com>
2026-02-23 19:57:51 +0100

    potential goatfix

--- partials/footer.js
+++ partials/footer.js
@@ -2,8 +2,8 @@ const footer = document.querySelector('footer');
 footer.innerHTML = '<a href="https://github.com/lundaTempo/lundaTempo.github.io">Källkod</a>';
 
 
+window.goatcounter = { endpoint: 'https://lundatempo.goatcounter.com/count' };
 const script = document.createElement('script');
-script.dataset.goatcounter = 'https://lundatempo.goatcounter.com/count';
 script.async = true;
 script.src = '//gc.zgo.at/count.js';
 footer.appendChild(script);
