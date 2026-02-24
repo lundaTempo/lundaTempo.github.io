9bda17990ac45f17c49304290677a690f72240dd
Joel Takahashi Olsson <joel.t.t.o@gmail.com>
2026-02-24 18:12:03 +0100

    styling

--- partials/footer.js
+++ partials/footer.js
@@ -1,5 +1,6 @@
+
 const footer = document.querySelector('footer');
-footer.innerHTML = '<a href="https://github.com/lundaTempo/lundaTempo.github.io">Källkod</a>';
+footer.innerHTML = '<a href="https://github.com/lundaTempo/lundaTempo.github.io">Källkod</a><br><br>';
 
 window.goatcounter = { endpoint: 'https://lundatempo.goatcounter.com/count' };
 const script = document.createElement('script');
